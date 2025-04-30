39b856fa2741105fe684d7cd500eaf70c01b6f06
shubh-rgb <tripathi1307shubh@gmail.com>
2025-04-30 15:06:42 +0530

    fix

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-import '../styles/App.css'; // Adjust if needed
+import Navbar from './Navbar';
+import '../styles/Layout.css'; // Adjust if needed
 
 const Layout = ({ children }) => {
   return (
     <div className="layout-container">
+      <Navbar />
       <Sidebar />
       {/* </aside> */}
       <div className="main-content">
